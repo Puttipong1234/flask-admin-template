3eebba9d3251b0e87e1cbbcffc0dd2b6f42a55f2
Puttipong1234 <Puttipong.lims@gmail.com>
2019-11-26 16:06:24 +0700

    "adding chart and ajax sell data"

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 
 app = Flask(__name__)
 
@@ -11,6 +11,10 @@ def table():
 def index():
     return render_template('index.html')
 
+@app.route('/api/sendmail/<userid>')
+def sending(userid):
+    req = request.form
+    print(req)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/demo/chart-area-demo.js
+++ static/js/demo/chart-area-demo.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [0, 15000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
     }],
   },
   options: {
@@ -116,3 +116,15 @@ var myLineChart = new Chart(ctx, {
     }
   }
 });
+
+
+// ajax data from here
+
+setTimeout(function() {
+  addData(myLineChart, [0, 25000, 15000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000], 0);
+}, 2000);
+
+function addData(chart, data, datasetIndex) {
+  chart.data.datasets[datasetIndex].data = data;
+  chart.update();
+}
